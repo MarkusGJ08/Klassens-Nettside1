d21ecb9e0c48386a61ae88d33d0b80c8ab340953
Markus Gjerde <gjemar0604@elev.tromsfylke.no>
2025-08-29 11:17:08 +0200

    oppdatering js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Henter knappen fra siden
+const btn = document.querySelector(".button");
+
+// Legger til en klikk-hendelse
+btn?.addEventListener("click", () => {
+  alert("Du trykket på knappen!");
+});
